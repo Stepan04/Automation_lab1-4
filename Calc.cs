68c7dec18961d543e20992f859264b04686ffd57
Stepan04 <114218948+Stepan04@users.noreply.github.com>
2023-10-10 23:10:35 +0300

    Add files via upload

---
+++ Calc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcLib
+{
+    public class Calc
+    {
+        public static double Mod(double a, double b)
+        {
+            double r;
+            r = a % b;
+            if (double.IsInfinity(r))
+                throw new OverflowException();
+            return r;
+           
+            
+        }
+   
+    }
+}
---
+++ CalcTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using CalcLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcLibTests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("System.Data.SqlClient", "Server = .\\SQLEXPRESS;Integrated Security = True;Database = Mod_for_calculator", "TestData", DataAccessMethod.Sequential)]
+
+        [TestMethod]
+
+        public void Calc_Mod_FromDB()
+        {
+            double a = Convert.ToDouble(TestContext.DataRow["a"]);
+            double b = Convert.ToDouble(TestContext.DataRow["b"]);
+            double r = Convert.ToDouble(TestContext.DataRow["r"]);
+            double actual;
+           
+          
+                actual = Calc.Mod(a, b);
+                Assert.AreEqual(r, actual);
+           
+        }
+
+
+        [TestMethod]
+        public void Mod_10mod3_1returned()
+        {
+            
+            //AAA
+            //Arrange
+            double a = 10;
+            double b = 3;
+            double expected = 1;
+            //Actual
+            double actual = Calc.Mod(a, b);
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+    }
+}
